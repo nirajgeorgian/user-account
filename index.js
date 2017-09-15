a9f52edb1e8269ca2a0000097d672544a3a5f1ca
ritimuz <ritimuz@gmail.com>
2017-09-16 00:37:09 +0530

    testing from remote

--- index.js
+++ index.js
@@ -2,5 +2,6 @@
 const app = require('./server/server');
 const config = require('./server/config/config');
 
+// app configuration here
 app.listen(config.port);
 console.log("Running on port " + config.port);
