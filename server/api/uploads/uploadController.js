71ac13f87419cf03c1fb83cc404fe104cc95924c
nirajgeorgian <nirajgeorgian01@gmail.com>
2017-09-19 18:39:14 +0530

    completed profile file upload option

--- server/api/uploads/uploadController.js
+++ server/api/uploads/uploadController.js
@@ -1,21 +1,49 @@
 "use strict";
+const fs = require('fs');
+const path = require('path');
+const crypto = require('crypto');
 const multer = require('multer');
+
+// globally allowed extname
+const extensionName = ['.pdf', '.jpg', '.jpeg', '.png', '.git', '.svg'];
 
 // multer options
 let storage = multer.diskStorage({
   destination: function(req, file, cb) {
-    cb(null, './uploads')
+    cb(null, './uploads');
   },
   filename: function(req, file, cb) {
-    cb(null, file.fieldname + '-' + Date.now())
+    crypto.randomBytes(10, function(err, buff) {
+      if (err) return err;
+      const fileName = buff.toString('hex') + '-' + Date.now() + path.extname(file.originalname);
+      cb(null, fileName);
+    });
   }
-})
+});
 
 // let upload = multer({storage: storage});
 exports.post = function(req, res, next) {
-  let upload = multer({storage: storage}).any();
+  let upload = multer({
+    storage: storage,
+    fileFilter: function(req, file, cb) {
+      var extName = path.extname(file.originalname);
+      if(extensionName.includes(extName)) {
+        return cb(null, true);
+      } else {
+        return res.status(409).json({
+          "success": "failure",
+          "statusCode": 409
+        });
+      }
+    }
+  }).any();
   upload(req, res, function(err) {
-    console.log(res);
-    res.send('file uploaded');
-  })
+    res.json({
+      "success": "success",
+      "mimetype": req.files[0].mimetype,
+      "filename": req.files[0].filename,
+      "path": req.files[0].path,
+      "size": req.files[0].size
+    });
+  });
 }
